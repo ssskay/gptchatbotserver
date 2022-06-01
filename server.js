adbab1d69828ae354fd6fab37e4681528a6e9d39
Sara Kay <sarakay@saras-mbp-3.kiewit.dartmouth.edu>
2022-05-31 23:04:04 -0400

    My first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5001;
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(require("./routes/user-responses"));
+// get driver connection
+const dbo = require("./db/conn");
+
+app.listen(port, () => {
+  console.log("a");
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+  });
+  console.log(`Server is running on port: ${port}`);
+});

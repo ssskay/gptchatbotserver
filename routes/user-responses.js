9ac54785cef625302566cac20dd42dbe9ca8d907
Sara Kay <sarakay@saras-mbp-3.kiewit.dartmouth.edu>
2022-05-31 22:51:40 -0400

    herokuuu

--- routes/user-responses.js
+++ routes/user-responses.js
@@ -13,36 +13,56 @@ const test = 1;
 // This help convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
 
+routes.route("/users/add").post(async (req, response) => {
+  let db_connect = dbo.getDb();
+
+  db_connect
+    .collection("users")
+    .insertOne({ date: new Date(), submitCount: 0 }, async (err, res) => {
+      if (err) throw err;
+      response.json({ _id: res.ops[0]._id });
+    });
+});
+
+// Return AI responses to the given userResponse.
+routes.route("/ai-responses").get(async (req, response) => {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const responses = (
+    await openai.createCompletion("text-davinci-002", {
+      prompt: "Human:" + req.query.userResponse + ".Chatbot:",
+      temperature: 1,
+      max_tokens: 60,
+      top_p: 0.9,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+      n: 3,
+    })
+  ).data.choices.map((choice) => choice.text);
+
+  response.json({ responses: responses });
+});
+
 // This section will help you create a new record.
 routes.route("/user-responses/add").post(async (req, response) => {
   let db_connect = dbo.getDb();
   let myobj = {
     userId: req.body.userId,
     user_response: req.body.userResponse,
+    ai_response: req.body.aiResponses,
+    user_choice_1: req.body.userChoice1,
+    feedback_1: req.body.feedback1,
+    user_choice_2: req.body.userChoice2,
+    feedback_2: req.body.feedback2,
+    feedback_3: req.body.feedback3,
   };
   db_connect.collection("user_responses").insertOne(myobj, async (err, res) => {
     if (err) throw err;
 
-    const configuration = new Configuration({
-      apiKey: process.env.OPENAI_API_KEY,
-    });
-    const openai = new OpenAIApi(configuration);
-
-    const responses = (
-      await openai.createCompletion("text-davinci-002", {
-        prompt: "Human:" + myobj.user_response + ".Chatbot:",
-        temperature: 1,
-        max_tokens: 60,
-        top_p: 0.9,
-        frequency_penalty: 0.5,
-        presence_penalty: 0,
-        n: 3,
-      })
-    ).data.choices.map((choice) => choice.text);
-
-    console.log(responses);
-
-    response.json({ responses: responses, _id: res.ops[0]._id });
+    response.json({ _id: res.ops[0]._id });
   });
 });
 
